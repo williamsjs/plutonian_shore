e92b144881554304928c3326f89400f30b36c36e
Scott Williams <williamsjs1224@gmail.com>
2015-09-18 11:32:58 -0400

    relocate background image to css. relocate nav elements to application.html

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,35 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+    if ($("#js-parallax-window").length) {
+        parallax();
+    }
+});
+
+$(window).scroll(function(e) {
+    if ($("#js-parallax-window").length) {
+        parallax();
+    }
+});
+
+function parallax(){
+    if( $("#js-parallax-window").length > 0 ) {
+        var plxBackground = $("#js-parallax-background");
+        var plxWindow = $("#js-parallax-window");
+
+        var plxWindowTopToPageTop = $(plxWindow).offset().top;
+        var windowTopToPageTop = $(window).scrollTop();
+        var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
+
+        var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
+        var windowInnerHeight = window.innerHeight;
+        var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
+        var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
+        var plxSpeed = 0.35;
+
+        plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
+    }
+}
