19df2600005d09b76170713172f2abaf22b77eea
Scott Williams <williamsjs1224@gmail.com>
2015-10-30 16:20:56 -0400

    get article class method to call private methods

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,24 +1,27 @@
 class Article < ActiveRecord::Base
 
 
-  #NPR_KEY
-  # path: http://api.npr.org/query?searchTerm=horror&output=json&apiKey=#ENV{'NPR_KEY'}
-  def self.ten_articles
+  def self.get_ten_articles
     10.times do |count|
-      generate_content(count)
+      title = get_articles['list']['story'][count]['title']['$text']
+      Article.create(name: title)
     end
   end
 
-  private
+  class << self
+    
+    private
+      def get_articles
+        content = HTTParty.get("http://api.npr.org/query?searchTerm=horror&output=json&apiKey=#{ENV['NPR_KEY']}")
+        JSON.parse(content)  #funky npr json format
+      end
 
-    def get_articles
-      content = HTTParty.get('http://api.npr.org/query?searchTerm=horror&output=json&apiKey=#ENV{'NPR_KEY'}')
-      JSON.parse(content)  #funky npr json format
-    end
+      def get_content(count)
+        # article = get_article so we don't have to keep repeating same function
+        title = get_articles['list']['story'][count]['title']['$text']
+        Article.create(title: title)
+      end
 
-    def generate_content(count)
-      # article = get_article so we don't have to keep repeating same function
-      title = get_articles['list']['story'][count]['title']['text']
+  end
 
-    end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,5 @@
 class Book < ActiveRecord::Base
   belongs_to :author
-  validates :publication_year, :author, presence: true
-
+  validates :publication_year, presence: true
 
 end
