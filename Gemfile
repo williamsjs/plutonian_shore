a8e52c9f180afc8b7bd2794aeea77858e1964c27
Scott Williams <williamsjs1224@gmail.com>
2015-10-30 11:27:05 -0400

    add wheneverize

--- Gemfile
+++ Gemfile
@@ -24,7 +24,9 @@ gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-require 'httparty'
+
+gem 'httparty'
+gem 'whenever', :require => false # automatically generate content
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,24 @@
 class Article < ActiveRecord::Base
 
+
   #NPR_KEY
+  # path: http://api.npr.org/query?searchTerm=horror&output=json&apiKey=#ENV{'NPR_KEY'}
+  def self.ten_articles
+    10.times do |count|
+      generate_content(count)
+    end
+  end
+
+  private
+
+    def get_articles
+      content = HTTParty.get('http://api.npr.org/query?searchTerm=horror&output=json&apiKey=#ENV{'NPR_KEY'}')
+      JSON.parse(content)  #funky npr json format
+    end
+
+    def generate_content(count)
+      # article = get_article so we don't have to keep repeating same function
+      title = get_articles['list']['story'][count]['title']['text']
+
+    end
 end
